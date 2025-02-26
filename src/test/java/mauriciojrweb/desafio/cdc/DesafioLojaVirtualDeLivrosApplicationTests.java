4f9268af5724d7240dc484293fc66bf47c379ef1
Mauricio Moreira Junior <mauricio.junior@iterative.com.br>
2025-02-26 11:37:05 -0300

    Primeiro commit

---
+++ src/test/java/mauriciojrweb/desafio/cdc/DesafioLojaVirtualDeLivrosApplicationTests.java
@@ -0,0 +1,13 @@
+package mauriciojrweb.desafio.cdc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioLojaVirtualDeLivrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
